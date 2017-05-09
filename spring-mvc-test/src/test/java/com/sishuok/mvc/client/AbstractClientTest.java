0575c466bcacef5753f14fa06c81a02d3f3c1e36
liusx <1014426766@qq.com>
2017-05-09 17:55:20 +0800

    1

---
+++ spring-mvc-test/src/test/java/com/sishuok/mvc/client/AbstractClientTest.java
@@ -0,0 +1,37 @@
+package com.sishuok.mvc.client;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 13-12-29
+ * <p>Version: 1.0
+ */
+public abstract class AbstractClientTest {
+
+    static RestTemplate restTemplate;
+    ObjectMapper objectMapper;//JSON
+    Jaxb2Marshaller marshaller;//XML
+    String baseUri = "http://localhost:8080/users";
+
+    @Before
+    public void setUp() throws Exception {
+        objectMapper = new ObjectMapper(); //需要添加jackson jar包
+
+        marshaller = new Jaxb2Marshaller(); //需要添加jaxb2实现（如xstream）
+        marshaller.setPackagesToScan(new String[]{"com.sishuok"});
+        marshaller.afterPropertiesSet();
+
+        restTemplate = new RestTemplate();
+        //restTemplate.setRequestFactory();
+    }
+}
